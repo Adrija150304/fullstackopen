b0a3e5289c077706eca2e8f943aadd0f22c5ff34
Adrija150304 <adrijapaul150304@gmail.com>
2025-07-26 01:58:31 +0530

    Completed 1.10 - Button and StatisticLine components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+// src/App.jsx
 import { useState } from 'react'
 
 const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -15,14 +25,16 @@ const Statistics = ({ good, neutral, bad }) => {
   const positive = (good / total) * 100
 
   return (
-    <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average.toFixed(1)}</p>
-      <p>positive {positive.toFixed(1)} %</p>
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average.toFixed(1)} />
+        <StatisticLine text="positive" value={positive.toFixed(1) + ' %'} />
+      </tbody>
+    </table>
   )
 }
 
@@ -33,12 +45,12 @@ const App = () => {
 
   return (
     <div>
-      <h1>give feedback</h1>
+      <h1>Give feedback</h1>
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
 
-      <h1>statistics</h1>
+      <h1>Statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
